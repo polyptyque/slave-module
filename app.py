895f34cd9e2b5b09f95c1e64ef3613d8ed607675
Arthur Violy <arthur@violy.net>
2017-04-13 14:58:37 +0200

    UDP socker server

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os.path
 import shutil
 import configparser
+import socket
 import requests
 
 # duplique le fichier de configuration par defaut
@@ -13,19 +14,33 @@ if not os.path.isfile('config.ini'):
 # charge le fichier de configuration
 config = configparser.ConfigParser()
 config.read('config.ini')
-print('Server Url : '+config['master']['url'])
 
 # variables
-url = config['master']['url']+'post'
+url = 'http://'+config['master']['hostname']+'/post'
 mod_id = config['module']['id']
+ip_master = config['udp']['ip_master']
+udp_port = int(config['udp']['port'])
+print('Server Url : '+url)
 
-# fichiers images
-files = [
-    ('a', ('test-a.jpg',open('test-a.jpg', 'rb'), 'image/jpg')),
-    ('b', ('test-b.jpg',open('test-b.jpg', 'rb'), 'image/jpg'))
-    ]
-# headers HTTP applicatif
-headers = {'x-mod-id': mod_id}
+def sendimages():
+    # fichiers images
+    files = [
+        ('a', ('test-a.jpg',open('test-a.jpg', 'rb'), 'image/jpg')),
+        ('b', ('test-b.jpg',open('test-b.jpg', 'rb'), 'image/jpg'))
+        ]
+    # headers HTTP applicatif
+    headers = {'x-mod-id': mod_id}
 
-r = requests.post(url, files=files, headers=headers)
-print(r.text)
+    r = requests.post(url, files=files, headers=headers)
+    print(r.text)
+
+# serveur UDP
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(('', udp_port))
+print("bind socket on port "+str(udp_port))
+
+while True:
+    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    print("received message:"+str(type(data)))
+    print(addr)
+    print(data)
