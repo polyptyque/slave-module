5174ce336f3340aaed0a08c319ed2c97d674cfaf
Arthur Violy <arthur@violy.net>
2017-04-19 23:48:56 +0200

    demo preview shot, with simulation mode

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import shutil
 import configparser
 import socket
 import requests
-import picamera
 import time
 import io
+import json
 
-# duplique le fichier de configuration par defaut
+# duplique le fichier de configuration par défaut
 # si le fichier de configuration n'existe pas
 if not os.path.isfile('config.ini'):
     print('Copy config from default...')
@@ -18,57 +18,81 @@ if not os.path.isfile('config.ini'):
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-# variables
+# variables par défaut
+runmode = config['app']['runmode']
+simulation = runmode == "simulation"
 master_base_url = 'http://'+config['master']['hostname']+':'+config['master']['port']
 url = master_base_url+'/post'
 mod_id = config['module']['id']
-ip_master = config['udp']['ip_master']
 udp_port = int(config['udp']['port'])
 print('Server Url : '+master_base_url)
 
-# declare la camera
-camera0 = picamera.PiCamera(0)
-camera1 = picamera.PiCamera(1)
-# lance la camera
-#camera0.start_preview()
-#camera1.start_preview()
+def updatemasterdata(h,p):
+    # update datas from master UDP configuration
+    master_base_url = 'http://'+h+':'+p
+    url = master_base_url+'/post'
 
-def sendimages(prefix):
+if not simulation :
+    import picamera
+
+    # declare la camera
+    camera0 = picamera.PiCamera(0)
+    camera1 = picamera.PiCamera(1)
+    # lance la camera
+    #camera0.start_preview()
+    #camera1.start_preview()
+
+def sendimages(id):
     # fichiers images
+
+    filename0 = id+'0.jpg'
+    filename1 = id+'1.jpg'
+    if not simulation :
+        src0 = filename0,
+        src1 = filename1
+    else :
+        src0 = 'test-a.jpg'
+        src1 = 'test-b.jpg'
+
     files = [
-        ('a', (prefix+'0.jpg',open(prefix+'0.jpg', 'rb'), 'image/jpg')),
-        ('b', (prefix+'1.jpg',open(prefix+'1.jpg', 'rb'), 'image/jpg'))
+        ('a', (filename0,open(src0, 'rb'), 'image/jpg')),
+        ('b', (filename1,open(src1, 'rb'), 'image/jpg'))
         ]
     # headers HTTP applicatif
-    headers = {'x-mod-id': mod_id,'x-img-prefix':prefix}
+    headers = {
+        'x-run-mod':runmode,
+        'x-mod-id': mod_id,
+        'x-shot-id':id
+    }
 
     r = requests.post(url, files=files, headers=headers)
     print(r.text)
 
-   
+def takeimages(id):
 
-def takeimages():
-    t = time.strftime('%Y-%m-%d_%H-%M-%S')
-    a = time.clock()
-    stream0 = io.BytesIO()
-    stream1 = io.BytesIO()
-    
-    #print("capture camera 0", time.clock())
-    #f0 = camera0.capture_continuous(stream0, format='jpeg', use_video_port=False)
-    #print("capture camera 1", time.clock())
-    #f1 = camera1.capture_continuous(stream1, format='jpeg', use_video_port=False)
-    #print("ok",time.clock());
-    
-    #savejpegstream(0,stream0);
-    #savejpegstream(1,stream1);
+    if not simulation :
 
-    img_prefix = 'img-'+t+'-cam-'
-    camera0.capture(img_prefix+'0.jpg', format='jpeg')
-    camera1.capture(img_prefix+'1.jpg', format='jpeg')
+        t = time.strftime('%Y-%m-%d_%H-%M-%S')
+        a = time.clock()
+        stream0 = io.BytesIO()
+        stream1 = io.BytesIO()
 
-    b = time.clock()
-    print('image shot in '+str(round((b-a)*1000))+'ms ')
-    sendimages(img_prefix)
+        #print("capture camera 0", time.clock())
+        #f0 = camera0.capture_continuous(stream0, format='jpeg', use_video_port=False)
+        #print("capture camera 1", time.clock())
+        #f1 = camera1.capture_continuous(stream1, format='jpeg', use_video_port=False)
+        #print("ok",time.clock());
+
+        #savejpegstream(0,stream0);
+        #savejpegstream(1,stream1);
+
+        camera0.capture(id+'0.jpg', format='jpeg')
+        camera1.capture(id+'1.jpg', format='jpeg')
+
+        b = time.clock()
+        print('image shot in '+str(round((b-a)*1000))+'ms ')
+
+    sendimages(id)
 
 # serveur UDP
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -79,6 +103,11 @@ print("bind socket on port "+str(udp_port))
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
     print("received message:"+str(type(data)))
+    message = json.loads(data.decode('utf-8'))
     print(addr)
-    print(data)
-    takeimages()
+    if message['action'] == 'shot' :
+        print("shot",message['id'])
+        takeimages(message['id'])
+    else :
+        print("action inconnue")
+
