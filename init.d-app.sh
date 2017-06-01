d7dd5bfa94accac664a94ad28048ed48ab1eeaad
Arthur Violy <arthur@violy.net>
2017-06-01 14:35:04 +0200

    init.d app shell script

--- init.d-app.sh
+++ init.d-app.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
+### BEGIN INIT INFO
+# Provides:          Polyptyque
+# Required-Start:    $all
+# Required-Stop:
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Start polyptyque slave-module python3 app
+### END INIT INFO
 
+echo "init polyptyque..."
 /home/pi/slave-module/app.py
