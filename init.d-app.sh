26817b29f2468db5a164cf4bf95e70f61cc6d670
Arthur Violy <arthur@violy.net>
2017-06-03 16:28:46 +0200

    init.d app daemon

--- init.d-app.sh
+++ init.d-app.sh
@@ -8,5 +8,51 @@
 # Short-Description: Start polyptyque slave-module python3 app
 ### END INIT INFO
 
-echo "init polyptyque..."
-/home/pi/slave-module/app.py
+DAEMON_SBIN=/home/pi/slave-module/app.py
+NAME=polyptyque-slave-module
+DESC="Polyptyque slave-module"
+PIDFILE=/var/run/polyptyque-slave-module.pid
+
+[ -x "$DAEMON_SBIN" ] || exit 0
+
+DAEMON_OPTS="-B -P $PIDFILE $DAEMON_OPTS"
+
+. /lib/lsb/init-functions
+
+case "$1" in
+  start)
+        log_daemon_msg "Starting $DESC" "$NAME"
+        start-stop-daemon --start --oknodo --quiet --exec "$DAEMON_SBIN" \
+                --pidfile "$PIDFILE" -- $DAEMON_OPTS >/dev/null
+        log_end_msg "$?"
+        ;;
+  stop)
+        log_daemon_msg "Stopping $DESC" "$NAME"
+        start-stop-daemon --stop --oknodo --quiet --exec "$DAEMON_SBIN" \
+                --pidfile "$PIDFILE"
+        log_end_msg "$?"
+        ;;
+  reload)
+        log_daemon_msg "Reloading $DESC" "$NAME"
+        start-stop-daemon --stop --signal HUP --exec "$DAEMON_SBIN" \
+                --pidfile "$PIDFILE"
+        log_end_msg "$?"
+        ;;
+  restart|force-reload)
+        $0 stop
+        sleep 8
+        $0 start
+        ;;
+  status)
+        status_of_proc "$DAEMON_SBIN" "$NAME"
+        exit $?
+        ;;
+  *)
+        N=/etc/init.d/$NAME
+        echo "Usage: $N {start|stop|restart|force-reload|reload|status}" >&2
+        exit 1
+        ;;
+esac
+
+exit 0
+
