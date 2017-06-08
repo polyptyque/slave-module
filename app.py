e364a4cd4b376f1ecaf2b760f70677a9a6fda874
Arthur Violy <arthur@violy.net>
2017-06-08 13:19:35 +0200

    configDefault_camera_options except

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ configDefault_camera_options = json.loads(json.dumps(dict(configDefault.items('c
 for key, value in configDefault_camera_options.items():
     try:
         config.get('camera', key)
-    except KeyError:
+    except:
         print("\tset default config -> " + key + " : " + value)
         config.set('camera', key, value)
 
