d01c81b30e1de2fa67ceb9055328db15fee681a0
Arthur Violy <arthur@violy.net>
2017-06-08 14:54:48 +0200

    resolution

--- app.py
+++ app.py
@@ -162,7 +162,7 @@ def update_camera_options(camera):
         camera.awb_mode = 'off'
 
         # Camera resolution
-        camera.resolution = (cam_height, cam_width)
+        camera.resolution = cam_width+'x'+cam_height
 
         # Led off
         # camera.led = 0
